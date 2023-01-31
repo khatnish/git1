c9d4a05616e3e38b52608c974e535e901389d741
khatnish <nishakhatiwada45@gmail.com>
2023-01-31 15:41:55 -0500

    first

--- git.java
+++ git.java
@@ -5,9 +5,188 @@ public class git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		int x=10;
-		int y =20;
-	}
+//		//without parameter without return
+//		Addition();
+//		
+//		//with parameter without return type
+//		
+//		AdditionA(3,4);
+//		//with parameter with return type
+//		int z = AdditionB(1,2);
+//		System.out.println(z);
+//		
+//
+//		
+//	}
+//	static void Addition() {
+//		System.out.println(9+5);
+//	}
+//	static void AdditionA(int a, int b) {
+//		System.out.println(a+b);
+//	}
+//	static int AdditionB(int x, int y) {
+//		return(x+y);
+//	}
 
+//		// comparision Operators
+//		System.out.println(3==3); //true
+//		System.out.println(3>=3); //true
+//		System.out.println(3<=3);//true
+//		System.out.println(3!=3);//false
+//		System.out.println(9==3);//false
+//		System.out.println(5<=3);//false
+//		System.out.println(7>=3);//true
+//		System.out.println(3>3);//false
+
+		// Logical operator
+		// And &&
+//		System.out.println(2==2&&2!=3);
+//		System.out.println(4<=3&&7<=3);
+//		System.out.println(5<6&&1!=1);
+//		System.out.println(5<=6&&7==2);
+
+		// Or ||
+//		System.out.println(2==2||2!=3);
+//		System.out.println(4<=3||7<=3);
+//		System.out.println(5<6||1!=1);
+//		System.out.println(5<=6||7==2);
+
+		// Not !
+//		System.out.println(!(3==3));
+//		System.out.println(!(3!=3));
+
+		// if else
+//		int numT = 45;
+//		if (numT>0&&numT<=10) {
+//			System.out.println("10% discount");
+//			
+//		}
+//		if(numT>10&&numT<=20) {
+//			System.out.println("20% discount");
+//			
+//		}
+//		if(numT>20) {
+//			System.out.println("30% discount");
+//		}
+//		
+//		int marks = 88;
+//		if (marks>=80) {
+//			System.out.println("Grade A");
+//			
+//		}
+//		if (marks >=60) {
+//			System.out.println("Grade B");
+//		}
+//	if(marks>=50) {
+//		System.out.println("Grade C");
+//		
+//	}
+//	else {
+//		System.out.println("Fail");
+//	}
+//		int marks = 88;
+//		if (marks >= 80) {
+//			System.out.println("Grade A");
+//
+//		} else if (marks >= 60) {
+//			System.out.println("Grade B");
+//		} else if (marks >= 50) {
+//			System.out.println("Grade C");
+//
+//		} else {
+//			System.out.println("Fail");
+//		}
+		//
+//     int x=5;
+//     int y=6;
+//     if(x>y) {
+//    	 System.out.println("x is greater");
+//    	 }
+//    	 else {
+//    		 System.out.println("y is greater");
+//    	 }
+		
+//		int x=9;
+//		int y=8;
+//		int z=7;
+//		if(x>y&&x>z) {
+//			System.out.println("x is greater");
+//			
+//		}
+//		else if (y>x && y>z) {
+//			System.out.println("y is greater");
+//			
+//		}
+//		else {
+//			System.out.println("z is greater");
+//		}
+		
+		
+		//switch case
+//		String city = "Lexinton";
+//		switch (city) {
+//		case "Lexington":
+//		case "Louisville":
+//			System.out.println("KY");
+//			break;
+//		case "Merion":
+//		case "Morganton":
+//			System.out.println("NC");
+////			default:
+////				System.out.println("Please put valid inputs");
+////				
+////		}
+//		
+//		int day = 5;
+//		switch (day) {
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//		case 3:
+//			System.out.println("Tuesday");
+//			break;
+//		case 4:
+//			System.out.println("Wednesday");
+//			break;
+//			
+//			
+//		}
+//			for (int x=0; x<=10; x++) {
+//				if(x==5) {
+//					continue;
+//					
+//				}
+//				System.out.println(x);
+//			}
+//			for (int x=2; x<=20; x=x+2) {
+//				System.out.println(x);
+//			}
+//			
+//			 int x=2;
+//			 while (x<=20) {
+//				 
+//			 System.out.println(x);
+//			 x=x+2;
+//			 }
+			 
+			 int x=0;
+			 while(x<=5) {
+				 if(x==3) {
+					 break;
+				 }
+				 System.out.println(x);
+				 x++;
+			 }
+			 
+			 
+			
+			
+					
+		
+		
+		
+     }
 }
